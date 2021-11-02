572086db0459caecfa1763b774c2d5757861bcae
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-11-02 14:15:24 -0700

    Initial project set up

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const path = require('path');
+
+app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('Hello from server!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
