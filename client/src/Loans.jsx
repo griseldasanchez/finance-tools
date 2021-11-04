852c92381b6723934a391109e347c9bee2e1a895
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-11-03 22:23:27 -0700

    Loan saving via table form

--- client/src/Loans.jsx
+++ client/src/Loans.jsx
@@ -5,6 +5,11 @@ import axios from 'axios';
 function Loans() {
 
   const [loans, setLoans] = useState([]);
+  const [provider, setProvider] = useState('');
+  const [term, setTerm] = useState(0);
+  const [principal, setPrincipal] = useState(0);
+  const [interest, setInterest] = useState(0);
+  const [monthlyPayment, setMonthlyPayment] = useState(0);
 
   useEffect(() => {
     axios.get('/loans')
@@ -12,28 +17,58 @@ function Loans() {
       .catch(err => err)
   }, [])
 
+  const saveLoan = () => {
+    event.preventDefault();
+    axios.post('/loans', {
+      'provider': provider,
+      'term': term,
+      'principal': principal,
+      'interest': interest,
+      'monthlyPayment': monthlyPayment
+    })
+    axios.get('/loans')
+      .then(response => setLoans(response.data))
+      .catch(err => err)
+  }
+
   return (
     <div>
-      <table className="loans-table">
-        <tr>
-          <th className="loans-table-header">ID#</th>
-          <th className="loans-table-header">Provider</th>
-          <th className="loans-table-header">Term in Years</th>
-          <th className="loans-table-header">Principal</th>
-          <th className="loans-table-header">Interest</th>
-          <th className="loans-table-header">Monthly Payment</th>
-        </tr>
-        {loans.map((loan, index) => (
-          <tr key={index}>
-              <td className="loans-table-header">{loan.loanid}</td>
-              <td className="loans-table-header">{loan.provider}</td>
-              <td className="loans-table-header">{loan.term} years</td>
-              <td className="loans-table-header">${loan.principal}</td>
-              <td className="loans-table-header">{loan.interest}%</td>
-              <td className="loans-table-header">${loan.monthlypayment}</td>
-            </tr>
-        ))}
-      </table>
+      <form>
+        <table className="loans-table">
+          <thead>
+            <th className="loans-table-cell">ID#</th>
+            <th className="loans-table-cell">Provider</th>
+            <th className="loans-table-cell">Term in Years</th>
+            <th className="loans-table-cell">Principal</th>
+            <th className="loans-table-cell">Interest</th>
+            <th className="loans-table-cell">Monthly Payment</th>
+          </thead>
+          {loans.map((loan, index) => (
+            <tbody key={index}>
+              <td className="loans-table-cell">{loan.loanid}</td>
+              <td className="loans-table-cell">{loan.provider}</td>
+              <td className="loans-table-cell">{loan.term} years</td>
+              <td className="loans-table-cell">${loan.principal}</td>
+              <td className="loans-table-cell">{loan.interest}%</td>
+              <td className="loans-table-cell">${loan.monthlypayment}</td>
+            </tbody>
+          ))} 
+            <td className="loans-table-cell">_</td>
+            <td className="loans-table-cell"><input required onChange={(event) => setProvider(event.target.value)}></input></td>
+            <td className="loans-table-cell"><input required onChange={(event) => setTerm(event.target.value)}></input></td>
+            <td className="loans-table-cell"><input required onChange={(event) => setPrincipal(event.target.value)}></input></td>
+            <td className="loans-table-cell"><input required onChange={(event) => setInterest(event.target.value)}></input></td>
+            <td className="loans-table-cell"><input required onChange={(event) => setMonthlyPayment(event.target.value)}></input></td>
+            <tfoot>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <button onClick={saveLoan} type="submit">Save Loan</button>
+            </tfoot>
+        </table>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -9,13 +9,37 @@ app.use(express.urlencoded({ extended: true }))
 
 const db = require('../database/connect.js');
 
+// Display Loan Records
 app.get('/loans', (req, res) => {
   db.client.query(`SELECT * FROM loans` , (err, data) => {
     if (err) {
-      // console.log('error in /loans');
+      // console.log('error in get /loans');
       res.send(err);
     } else {
-      console.log('data in /loans', data.rows);
+      // console.log('data in get /loans', data.rows);
+      res.send(data.rows);
+    }
+  })
+});
+
+// Add New Loan
+app.post('/loans', (req, res) => {
+  console.log('body', req.body)
+  let provider = req.body.provider;
+  let term = req.body.term;
+  let principal = req.body.principal;
+  let interest = req.body.interest;
+  let monthlyPayment = req.body.monthlyPayment;
+  db.client.query(`
+      INSERT INTO loans (provider,principal,interest,monthlyPayment,term) 
+      VALUES('${provider}', ${principal}, ${interest}, ${monthlyPayment}, ${term})` 
+    , (err, data) => {
+    if (err) {
+      // console.log('error in post /loans');
+      res.send(err);
+    } else {
+      console.log('data in post /loans', data.rows);
+      console.log('info', data);
       res.send(data.rows);
     }
   })
